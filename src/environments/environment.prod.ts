1e641d8a81d4bb5491e62f5e3d36a5b75d99b8e6
Giorgio Passoni <giorgio.passoni@gmail.com>
2026-07-09 21:10:06 +0200

    Update API_URL to camelCase format in production environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // Public Laravel backend origin.
-  API_URL: 'https://api.gigsaw.it',
+  apiUrl: 'https://api.gigsaw.it',
   apiPath: '/api/v1',
   googleAuthPath: '/auth/google/redirect',
   mapboxAccessToken: '',
